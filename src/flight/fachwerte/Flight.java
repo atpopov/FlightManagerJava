e9872ba826fa054714a02764a7945d1ae97847ad
taisan993 <taisan993@gmail.com>
2022-08-16 00:05:37 +0300

    First

--- src/flight/fachwerte/Flight.java
+++ src/flight/fachwerte/Flight.java
@@ -7,15 +7,17 @@ public final class Flight
     private final String _arrivalLocation;
     private final Date _departureTime;
     private final Date _arrivalTime;
+    private final Plane _plane;
 
     public Flight(int id, String departureLocation, String arrivalLocation,
-            Date departureTime, Date arrivalTime)
+            Date departureTime, Date arrivalTime, Plane plane)
     {
         _id = id;
         _departureLocation = departureLocation;
         _arrivalLocation = arrivalLocation;
         _departureTime = departureTime;
         _arrivalTime = arrivalTime;
+        _plane = plane;
     }
 
 }
