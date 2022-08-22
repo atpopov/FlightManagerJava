a72b33ea070f732772eecd3d4ebbdba7ff48181d
taisan993 <taisan993@gmail.com>
2022-08-22 18:23:30 +0300

    Commit Staged

--- src/flight/werkzeuge/FlightListWerkzeugUI.java
+++ src/flight/werkzeuge/FlightListWerkzeugUI.java
@@ -1,6 +1,34 @@
 package flight.werkzeuge;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class FlightListWerkzeugUI
 {
 
+    private static final String TITLE = "Flight Manager";
+
+    private JFrame _frame;
+    private JButton _endButton;
+
+    public FlightListWerkzeugUI()
+    {
+        _frame = new JFrame(TITLE);
+        _frame.setDefaultCloseOperation(_frame.EXIT_ON_CLOSE);
+        _frame.getContentPane()
+            .setLayout(new BorderLayout());
+    }
+
+    public void showWindow()
+    {
+        _frame.setSize(1200, 900);
+        _frame.setVisible(true);
+    }
+
+    public void closeWindow()
+    {
+        _frame.dispose();
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,4 @@
 module flightManager
 {
+    requires java.desktop;
 }
